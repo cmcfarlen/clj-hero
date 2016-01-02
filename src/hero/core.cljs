fdc8a12f1479a5ea604ae7912525d8d1ac45014d
Chris McFarlen <chris@mcfarlen.us>
2016-01-02 12:52:34 -0600

    add camera and viewport.  transfrom from world to view on render.

--- src/hero/core.cljs
+++ src/hero/core.cljs
@@ -6,35 +6,96 @@
 
 (enable-console-print!)
 
+
+(defn camera-setup
+  "Setup the camera with an origin, viewable width and height and a viewport"
+  [[x y w h] [vx vy vw vh]]
+  (let [aspect-ratio (/ vw vh)
+        h' (/ w aspect-ratio)
+        sw (/ vw w)
+        sh (/ vh h')]
+    (println [sw sh] "h'" h')
+    {:viewport [vx vy vw vh]
+     :origin [x y]
+     :scale [sw sh]}))
+
+(defn camera-pan
+  [cam [x y]]
+  (update-in cam [:origin] (fn [[cx cy]] [(+ cx x) (+ cy y)])))
+
 (defn game-init
   []
-  {:thing [100 100 10 10]})
+  {:camera (camera-setup [0.0 0.0 100.0 100.0] [0 0 (/ 1920 2) (/ 1080 2)])
+   :thing {:p [25 25]}})
 
 (defonce game-state (atom (game-init)))
 
+
+(comment
+
+ (swap! game-state update-in [:camera] camera-pan 10 10)
+ )
+
 (defn game-update
   [world input]
-  (let [down (-> input :keys :down)]
+  (let [down (-> input :keys :down)
+        mouse-down (-> input :mouse :down)]
     (cond-> world
       (down keys/a)
-      (update-in [:thing 0] dec)
+      (update-in [:thing :p 0] - 0.5)
       (down keys/d)
-      (update-in [:thing 0] inc)
+      (update-in [:thing :p 0] + 0.5)
       (down keys/s)
-      (update-in [:thing 1] inc)
+      (update-in [:thing :p 1] - 0.5)
       (down keys/w)
-      (update-in [:thing 1] dec))))
+      (update-in [:thing :p 1] + 0.5)
+      (mouse-down 0)
+      (update-in [:camera] camera-pan (-> input :mouse :Δp))
+      )
+
+    ))
+
+(defn setup-viewport
+  [ctx {:keys [viewport origin scale] :as cam}]
+  (let [[ox oy] origin
+        [sx sy] scale]
+
+  (.translate ctx ox oy)
+  (.translate ctx 0 (/ 1080 2))
+  (.scale ctx sx (- sy))))
+
 
 (defn game-render
   [ctx world]
   (let [canvas (.-canvas ctx)
         width (.-width canvas)
-        height (.-height canvas)]
+        height (.-height canvas)
+        [scale-x scale-y] (-> world :camera :scale)
+        ]
+    (.resetTransform ctx)
+
     (set! (.-fillStyle ctx) "#aaa")
     (.fillRect ctx 0 0 width height)
+
+    (setup-viewport ctx (:camera world))
+
+    (set! (.-strokeStyle ctx) "red")
+    (set! (.-lineWidth ctx) (/ 1.0 scale-x))
+    (.beginPath ctx)
+    (.moveTo ctx 0 -1000)
+    (.lineTo ctx 0 1000)
+    (.stroke ctx)
+
+    (set! (.-strokeStyle ctx) "blue")
+    (.beginPath ctx)
+    (.moveTo ctx -1000 0)
+    (.lineTo ctx 1000 0)
+    (.stroke ctx)
+
+
     (set! (.-fillStyle ctx) "#000")
-    (let [[x y w h] (:thing world)]
-      (.fillRect ctx x y w h))))
+    (let [[x y] (-> world :thing :p)]
+      (.fillRect ctx x y 10 10))))
 
 (def game-context (.getContext (.getElementById js/document "game") "2d"))
 
